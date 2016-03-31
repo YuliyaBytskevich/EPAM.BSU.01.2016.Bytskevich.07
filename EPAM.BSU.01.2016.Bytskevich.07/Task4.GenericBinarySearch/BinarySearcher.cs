03317323b9b72b55a4c07dadfd201c55e95a9963
Yuliya Bytskevich <yuliya.bytskevich@gmail.com>
2016-04-01 00:46:59 +0300

    T4. BinarySearcher & unit-tests added

---
+++ EPAM.BSU.01.2016.Bytskevich.07/Task4.GenericBinarySearch/BinarySearcher.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4.GenericBinarySearch
+{
+    public static class BinarySearcher
+    {
+        public static int? Search<T>(T[] sortedSource, T toBeFound, Comparer<T> comparer = null)
+        {
+            int? result = null;
+            if (sortedSource == null)
+                throw new ArgumentNullException(nameof(sortedSource));
+            if (sortedSource.Length == 0)
+                throw new ArgumentException("Source collection must not be empty");
+            int lowerIndex = 0;
+            int higherIndex = sortedSource.Length;
+            int middleIndex;
+            if (comparer == null)
+                comparer = Comparer<T>.Default;
+            if (comparer.Compare(toBeFound, sortedSource[lowerIndex]) < 0)
+                throw new ArgumentException("Search parameter is out of source collection range (it is less than min element)");
+            if (comparer.Compare(sortedSource[higherIndex-1], toBeFound) < 0)
+                throw new ArgumentException("Search parameter is out of source collection range (it is greater than max element)");
+            if (toBeFound.Equals(sortedSource[lowerIndex]))
+                return lowerIndex;
+            if (toBeFound.Equals(higherIndex-1))
+                return higherIndex-1;
+            while (lowerIndex < higherIndex)
+            {
+                middleIndex = (higherIndex - lowerIndex) / 2 + lowerIndex;
+                if (comparer.Compare(toBeFound, sortedSource[middleIndex]) <= 0)
+                    higherIndex = middleIndex;
+                else
+                    lowerIndex =middleIndex + 1;
+            }
+            if (comparer.Compare(sortedSource[lowerIndex], toBeFound) == 0)
+                return lowerIndex;
+            return null;
+        }
+    }
+}
---
+++ EPAM.BSU.01.2016.Bytskevich.07/Task4.GenericBinarySearchTests/BinarySearcherClassTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Task4.GenericBinarySearch;
+
+namespace Task4.GenericBinarySearchTests
+{
+    [TestFixture]
+    public class BinarySearcherClassTests
+    {
+        private int[] intPositiveArray = {1, 2, 3, 4, 5, 6, 7, 8, 9, 11};
+        private int[] intNullArray = {0, 0, 0, 0};
+        private double[] doubleNegativeArray = {-6.6666D, -5.5D, -4D, -3.3333D, -2.22D, -1.111111D};
+        private string[] stringArray = {"apple", "bell", "cinnamon", "direction", "eagle"};
+
+        public IEnumerable<TestCaseData> TestDataWithDefaultComparers
+        {
+            get
+            {
+                yield return new TestCaseData(intPositiveArray, 7).Returns(6);
+                yield return new TestCaseData(intPositiveArray, 1).Returns(0);
+                yield return new TestCaseData(intPositiveArray, 10).Returns(null);
+                yield return new TestCaseData(intPositiveArray, -10).Throws(typeof(ArgumentException));
+                yield return new TestCaseData(intPositiveArray, 1000).Throws(typeof(ArgumentException));
+                yield return new TestCaseData(intNullArray, 0).Returns(0);
+                yield return new TestCaseData(doubleNegativeArray, -4D).Returns(2);
+                yield return new TestCaseData(doubleNegativeArray, -4.55D).Returns(null);
+                yield return new TestCaseData(stringArray, "bells").Returns(null);
+                yield return new TestCaseData(stringArray, "direction").Returns(3);
+                yield return new TestCaseData(stringArray, "a").Throws(typeof (ArgumentException));
+                yield return new TestCaseData(stringArray, "fairy").Throws(typeof(ArgumentException));
+            }
+        }
+
+        [Test, TestCaseSource(nameof(TestDataWithDefaultComparers))]
+        public int? SearchWithDefaultComparer<T>(T[] sortedSource, T toBeFound)
+        {
+            return BinarySearcher.Search(sortedSource, toBeFound);
+        }
+
+    }
+}
