56d6e99a9914b4cf1875dc5355ea6fb07bc2f8a5
Yuliya Bytskevich <yuliya.bytskevich@gmail.com>
2016-03-31 18:54:14 +0300

    T2. Fibonacci sequence counter created

---
+++ EPAM.BSU.01.2016.Bytskevich.07/Task2.FibonacciSequence/FibonacciCounter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.FibonacciSequence
+{
+    public static class FibonacciCounter 
+    {
+        public static IEnumerable<int> GetSequence(int length)
+        {
+            if (length < 0)
+                throw new ArgumentException("Sequence lenght must be only positive integer value");
+            int penultimateNumber = 0;
+            int lastNumber = 1;
+            if (length >= 0)
+                yield return 0;       
+            if (length >= 1)
+                yield return 1;
+            if (length >= 2)
+                for (int i = 2; i <= length; i++)
+                {
+                    int currentNumber;
+                    yield return currentNumber = penultimateNumber + lastNumber;
+                    penultimateNumber = lastNumber;
+                    lastNumber = currentNumber;
+                }
+        } 
+    }
+}
